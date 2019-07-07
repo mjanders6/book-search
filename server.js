ea4ec9fb8a4d763460c7cac4c3a5c02b01fa4522
Mike <mjanders6@gmail.com>
2019-07-07 16:24:27 -0700

    switched db connections

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const { join } = require('path')
 const express = require('express')
 const app = express()
-const mongoDB = process.env.MONGODB_URI || 'mongodb://localhost/googlebooks'
+const mongoDB = 'mongodb://localhost/googlebooks' || process.env.MONGODB_URI
 
 app.use(express.static(join(__dirname, 'client', 'build')))
 app.use(express.urlencoded({ extended: true }))
@@ -10,6 +10,6 @@ app.use(express.json())
 
 require('./routes')(app)
 
-require('mongoose').connect('mongodb://localhost/googlebooks', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
+require('mongoose').connect(mongoDB, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
     .then(_ => app.listen(process.env.PORT || 3001))
     .catch(e => console.log(e))
