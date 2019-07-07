83ceb7569d7e1648c1f6fb352acdcfb09cb3b6cf
Mike <mjanders6@gmail.com>
2019-07-07 15:38:26 -0700

    update

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use(express.json())
 
 require('./routes')(app)
 
-require('mongoose').connect(mongoDB, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
+require('mongoose').connect('mongodb://localhost/googlebooks', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
     .then(_ => app.listen(process.env.PORT || 3001))
     .catch(e => console.log(e))
