aaf799985f71fea6445b74ab38f9d6f82de6bfaa
Mike <mjanders6@gmail.com>
2019-07-07 19:27:08 -0700

    better NavBar and Home page

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './NavBar.css'
 import { Link } from 'react-router-dom'
-import { Collapse, Navbar, NavbarBrand, Nav, NavItem } from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem} from 'reactstrap';
 
-const NavB = () => {
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
 
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
     return (
       <div>
         <Navbar color="light" light expand="md">
-          <NavbarBrand href='/'>Book Worm</NavbarBrand>
-          <Collapse navbar>
+          <NavbarBrand href="/">Book Worm</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
                 <Link to="/Books">Book Search</Link>
@@ -21,7 +35,8 @@ const NavB = () => {
           </Collapse>
         </Navbar>
       </div>
-    )
+    );
+  }
 }
 
-export default NavB
+export default NavBar
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Jumbotron, Container } from 'reactstrap';
+import { Link } from 'react-router-dom'
 
+const Home = (props) => {
+  return (
+    <div>
+      <Jumbotron fluid>
+        <Container fluid>
+          <h1 className="display-3">Aloha Book Worm!</h1>
+          <p className="lead">Search for a book that you can sit back and read</p>
+          <Link to="/Books">Follow this link to find a good book</Link>
+        </Container>
+      </Jumbotron>
+    </div>
+  );
+};
 
-const Home = _ => {
-    return (
-        <>
-            <h1>Aloha Book Worm</h1>
-        </>
-    )
-}
-
-
-export default Home
+export default Home;
